e3e6be4bb11c496ef2dfa421b5d067fd7bef4d38
Alexander Martins <alexandermartins123@icloud.com>
2023-01-07 10:09:41 -0600

    done

---
+++ github.js
@@ -0,0 +1,25 @@
+class Github {
+  constructor() {
+    this.client_id = "7d48f953aa1a164f1ba0";
+    this.client_secret = "a8703a4cbcfa0a10bda52125b4388dc02124c531";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
